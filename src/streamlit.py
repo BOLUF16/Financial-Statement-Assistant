1affabbfe733f0100d8731368d08b1bea328802a
BOLUF16 <olumodejibolu@gmail.com>
2024-11-12 22:07:32 +0700

    updated the streamlit user interface

--- src/streamlit.py
+++ src/streamlit.py
@@ -1,14 +1,24 @@
 import streamlit as st
 import requests
+import time
+import random
 
 # List of available models for user to select
 models = [
     # "llama-3.1-405b-reasoning",
     "llama-3.1-70b-versatile",
     "llama-3.1-8b-instant",
-    "mixtral-8x7b-32768"
+    "mixtral-8x7b-32768",
+    
 ]
-
+list_of_inputs = [
+    "How can i help you?",
+    "How can i assist you today?",
+    "What's on your mind?",
+    "Lets get cracking?",
+    "Is there anything i can help with?",
+    "I'm Batman!",
+]
 # Backend URLs for file upload and chat generation services
 BACKEND_URL = "http://127.0.0.1:5000" # API for file upload and chat generation
 
@@ -20,7 +30,17 @@ def upload_files(files):
     response = requests.post(url, files=files_data)
     return response.json()
 
+
+def chat_response(func):
+   def wrapper(question, model, temperature, session_id):
+       response= func(question, model, temperature, session_id)
+       for word in response.split():
+            yield word + " "
+            time.sleep(0.05)
+   return wrapper
+
 # Function to generate chat response by interacting with backend
+@chat_response
 def generate_chat(question, model, temperature, session_id):
     url = f"{BACKEND_URL}/generate"
     payload = {
@@ -33,33 +53,40 @@ def generate_chat(question, model, temperature, session_id):
     response = requests.post(url, json=payload)
     return response.text
 
+
 def main():
     # Title of the app
-    st.markdown("<h1 style='text-align: center;'>Financial Document Assistant</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center;'>Financial Document Assistant 💼</h1>", unsafe_allow_html=True)
     
-    # Centered image in a column layout
-    col1, col2, col3 = st.columns([1,2,1])
-    with col2:
-     st.image("https://th.bing.com/th/id/OIP.O8mZ0XXXbskfUKnSfRV11gHaEi?rs=1&pid=ImgDetMain", use_column_width=True)
-
-    # Initialize session state to store chat history
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = [] # Initialize an empty chat history
-
     
+    for chat_history in st.session_state.chat_history:
+        if chat_history['role'] == "user":
+            with st.chat_message(chat_history["role"], avatar= "👨"):
+                st.markdown(chat_history["content"])
+        else:
+            with st.chat_message(chat_history["role"], avatar= "🤖"):
+                st.markdown(chat_history["content"])
     # Sidebar settings
     with st.sidebar:
-        st.header("Settings")
+        st.markdown("## ⚙ Settings")
+        st.markdown("---")
         
         # Dropdown to select the model
-        model = st.selectbox("Select model", models)  
+        st.markdown("#### 🧠 AI Model")
+        model = st.selectbox("Select AI model", models) 
          # Slider for selecting temperature (controls randomness in LLM responses)
-        temperature = st.slider("Temperature", 0.0, 1.0, 0.5)
+        st.markdown("### 🌡 Temperature")
+        temperature = st.slider("Temperature", min_value= 0.0, max_value= 1.0, step= 0.1, help = "generates text with more variety, can lead to hallucinations on higher numbers")
          # Text input for session ID (used to track user conversations)
-        session_id = st.text_input("Enter session id")
+        st.markdown("### 🔗 Session Id")
+        session_id = st.text_input("Enter session id", help="used for tracking user conversation")
+
+        st.markdown("---")
         
          # Section for document upload
-        st.header("Document Upload")
+        st.markdown("## 📂 Document Upload")
         uploaded_files = st.file_uploader("Choose files", accept_multiple_files=True) # File uploader for multiple files
         if st.button("Process Documents"):# Button to trigger file upload process
             if uploaded_files : # Check if files are uploaded
@@ -72,34 +99,17 @@ def main():
             else:
                 st.warning("Please upload files") #Show warning if no files are uploaded
         
-         # Display chat history in the sidebar
-        st.header("Chat History")
-        response_container = st.container()
-        with response_container:
-             # Loop through chat history and display in reverse order (latest first)
-            for chat in reversed(st.session_state.chat_history):
-                st.markdown(f"""
-                <div style="background-color: #000000; padding:10px; border-radius:5px;">
-                    <p style="font-family:Arial, sans-serif;"><strong>You:</strong> {chat['human']}</p>
-                    <p style="font-family:Arial, sans-serif;"><strong>Bot:</strong> {chat['Ai']}</p>
-                </div>
-                """, unsafe_allow_html=True)
     
-     # Main content section for chat interaction
-    st.header("Chat Generation")
-    question = st.text_input("Enter your question")
-    
+    if prompt := st.chat_input(random.choice(list_of_inputs)):
+        with st.chat_message("user", avatar= "👨"):
+            st.markdown(prompt)
+        st.session_state.chat_history.append({"role": "user", "content": prompt})
+        with st.chat_message("assistant", avatar="🤖"):
+            response =generate_chat(prompt, model, temperature, session_id)
+            st.write_stream(response)
+        st.session_state.chat_history.append({"role": "assistant", "content": response})
 
-    if st.button("Generate Response"):# Button to trigger chat generation
-        if question:# Ensure that a question is provided
-            with st.spinner("Generating response..."): # Show spinner while generating response
-                response = generate_chat(question, model, temperature, session_id) # Call the generate_chat function
-                st.write("Response:", response)# Display the response
-                 # Append the interaction to the chat history stored in session state
-                st.session_state.chat_history.append({'human': question, 'Ai': response})
-                
-        else:
-            st.warning("Please enter a question and ensure you have processed documents for the project") # Warn user if no question is entered
+
     
 
 if __name__ == "__main__":
